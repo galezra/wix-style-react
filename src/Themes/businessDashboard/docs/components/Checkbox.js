6b957f26e366b2d0b9205046294e36933338abd9
shelly-goldblit <shellygo@wix.com>
2021-01-06 18:28:21 +0200

    update storybook

--- src/Themes/businessDashboard/docs/components/Checkbox.js
+++ src/Themes/businessDashboard/docs/components/Checkbox.js
@@ -2,20 +2,6 @@ export const playground = `
 <ThemeProvider theme={theme()}>
 <Layout>
   <Cell>
-    <Checkbox selectionArea="hover" size="small">Unchecked With Selesction Area</Checkbox>
-  </Cell>
-  <Cell>
-    <Checkbox selectionArea="hover" checked size="small">Checked With Selesction Area</Checkbox>
-  </Cell>
-  <Cell>
-    <Checkbox selectionArea="hover" checked size="small" disabled>
-      Checked Disabled With Selesction Area
-    </Checkbox>
-  </Cell>
-  <Cell>
-    <Checkbox size="small" selectionArea="hover" disabled>Disabled With Selesction Area</Checkbox>
-  </Cell>
-  <Cell>
   <Checkbox size="small">Unchecked</Checkbox>
 </Cell>
 <Cell>
