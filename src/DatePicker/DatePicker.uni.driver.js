2766b36c312f741b76d771216bfad2e3e8a34a1d
roibWix <roib@wix.com>
2021-01-06 15:44:26 +0200

    temp docs has been added

--- src/DatePicker/DatePicker.uni.driver.js
+++ src/DatePicker/DatePicker.uni.driver.js
@@ -18,11 +18,24 @@ export const datePickerUniDriverFactory = (base, body) => {
     open: async () => await inputDriver.focus(),
     getWidth: () => base._prop('style').then((style) => style.width),
   };
-
+  // TODO: needs to be fixed, autodocs doesn't generate the docs below.
   return {
+    /**
+     * Input Unidriver methods
+     */
     inputDriver,
+    /**
+     * Calender Unidriver methods
+     */
     calendarDriver,
+    /**
+     * Checks whether the component found with the given data hook.
+     * @returns {Promise<boolean>}
+     */
     exists: driver.exists,
+    /**
+     * Contains 'exists', 'open' and 'getWidth' methods
+     */
     driver
   }
 };
